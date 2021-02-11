803d836711418eb5c8d819fa097f3346789b3fa5
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-02-11 11:40:29 -0600

    setup backend server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express'); 
-const app = expres(); 
+const app = express(); 
 require('dotenv').config; 
+
+app.use(express.json()); 
+
+
+const port = process.env.PORT || 5000; 
+app.listen(port, () => {
+    console.log('Server listening on port', port)
+})
