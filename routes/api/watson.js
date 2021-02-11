80562cc08858a10adb368dcfa5d1ce08960ae846
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-02-11 12:52:41 -0600

    debug get request to server

--- routes/api/watson.js
+++ routes/api/watson.js
@@ -8,23 +8,23 @@ const {IamAuthenticator} = require('ibm-watson/auth')
 //1. Authenticate
 
 const authenticator = new IamAuthenticator({
-    apikey:process.env.WATSON_ASSISTANT_APIKEY,
+    apikey: process.env.WATSON_ASSISTANT_APIKEY,
 }); 
 
 //2. Connect Assistant
 const assistant = new AssistantV2({
-    version: '2021-02-11', 
+    version: '2019-02-28', 
     authenticator: authenticator,
     url: process.env.WATSON_ASSISTANT_URL
 })
 
 //3. Route to handle tokens 
-router.get('/session', async (req, res) => {
+router.get("/session", async (req, res) => {
     try{
         const session = await assistant.createSession({
             assistantId: process.env.WATSON_ASSISTANT_ID
         })
-        res.json(session('result'))
+        res.json(session['result'])
     }catch(err){
         res.send("There was an error processing your request")
         console.log(err); 
