c5e32c9d8343acec3897465bb2ace0ccdae916e0
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-02-11 16:02:06 -0600

    add handlemessage function logging input to console through state

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,13 +1,21 @@
 import React, {useState} from 'react'
 
 export const Chat = () => {
+    // Handle user's message
     const [message, setMessage] = useState(''); 
-    
+    const handleClick = async (e) => {
+        const code = e.keyCode || e.which; 
+
+        if(code===13){
+            console.log(message)
+            setMessage(""); 
+        }
+    }
     return (
         <div className="chat">
             <h1>Neumann the Chatbot</h1>
             <div>Messages go here</div>
-            <input id="chatBox"></input>
+            <input id="chatBox" onChange={(e)=>setMessage(e.target.value)} onKeyPress={handleClick} value={message}></input>
         </div>
     )
 }
